4d55883299e5eb7aee9b9e437157fbb77a5a7c21
Anton Lee <antoshalee@gmail.com>
2014-12-18 11:08:10 +0300

    Begin work

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in uniform_groups.gemspec
+gemspec
---
+++ lib/uniform_groups.rb
@@ -0,0 +1,48 @@
+# source must be a Hash with Array values
+#   hash = {
+#     "A" => %w(Armenia Austria),
+#     "B" => %w(Belgium),
+#     "C" => %w(Canada China Cuba)
+#   }
+#   groups = UniformGroups.new(hash, 2)
+#   groups.each { |group| do_something_with(group) }
+class UniformGroups
+  VERSION = "0.0.1"
+
+  include Enumerable
+
+  def initialize(hash, groups_number, options = {})
+    @source = hash.dup
+    @groups_number = groups_number
+    @header_ratio = options[:header_ratio] ||= 0
+  end
+
+  def each
+    group = {}
+    cur_count = index = 0
+
+    @source.each do |key, value|
+      cur_count += value.length + @header_ratio
+      group.merge!(key => value)
+
+      if cur_count > (average_weight * (index + 1))
+        yield(group)
+        group = {}
+        index += 1
+      end
+    end
+
+    yield(group) if index < @groups_number
+  end
+
+  private
+
+  def total_weight
+    headers_addition = @source.length * @header_ratio
+    @total_weight ||= @source.sum { |key, arr| arr.length } + headers_addition
+  end
+
+  def average_weight
+    total_weight / @groups_number.to_f
+  end
+end
